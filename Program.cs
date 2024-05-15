7e25a943b4b02a0667a142e2c9f63115e6bbb7a1
Daina Muiruri <61407935+DAINAWAMBUI@users.noreply.github.com>
2024-05-15 08:12:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            Console.WriteLine(a * b);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a % b);
+            Console.WriteLine(a / b);
+            
+        }
+    }
+}
